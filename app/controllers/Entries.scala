186de687ca3f575e2d13e2db1deae3eb0ec797ee
Vlastimil Menčík <vlastimil.mencik@etnetera.cz>
2014-02-09 11:29:27 +0100

    adding entries

--- app/controllers/Entries.scala
+++ app/controllers/Entries.scala
@@ -4,12 +4,19 @@ import play.api.mvc.Controller
 import model.EntryRepositoryComponent
 import model.InMemoryEntryRepositoryComponent
 import play.api.mvc.Action
+import model.Entry
 
 trait EntriesController {
   self: Controller with EntryRepositoryComponent =>
 
-  def addEntry = Action {
-    NotImplemented
+  def addEntry = Action { request =>
+    request.body.asText match {
+      case None | Some("") => BadRequest
+      case Some(body) =>
+        entryRepository.add(Entry(body))
+        Ok
+    }
+
   }
 }
 
--- test/controllers/EntriesTest.scala
+++ test/controllers/EntriesTest.scala
@@ -6,6 +6,8 @@ import model.InMemoryEntryRepositoryComponent
 import play.api.mvc.Controller
 import play.api.test.Helpers._
 import play.api.test.FakeRequest
+import model.Entry
+import play.api.libs.json.JsString
 
 class EntriesTest extends FunSpec with Matchers {
 
@@ -14,10 +16,29 @@ class EntriesTest extends FunSpec with Matchers {
     with InMemoryEntryRepositoryComponent
 
   describe("Entries controller") {
-    val c = new TestEntries
 
     it("should contain posted entry") {
+      val c = new TestEntries
       status(c.addEntry(FakeRequest().withTextBody("neco"))) should be(OK)
+      c.entryRepository.findAll should be(Seq(Entry("neco")))
+    }
+
+    it("should not accept empty entry") {
+      val c = new TestEntries
+      status(c.addEntry(FakeRequest().withTextBody(""))) should be(BAD_REQUEST)
+      c.entryRepository.findAll should be(Seq())
+    }
+
+    it("should not accept request with no body") {
+      val c = new TestEntries
+      status(c.addEntry(FakeRequest())) should be(BAD_REQUEST)
+      c.entryRepository.findAll should be(Seq())
+    }
+
+    it("should not accept request with json body") {
+      val c = new TestEntries
+      status(c.addEntry(FakeRequest().withJsonBody(JsString("neco")))) should be(BAD_REQUEST)
+      c.entryRepository.findAll should be(Seq())
     }
   }
 }
