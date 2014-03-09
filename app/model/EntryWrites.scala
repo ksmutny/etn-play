4f5fa725d75025c92bd5667ec05db886ab4789c3
Vlastimil Menčík <vlastimil.mencik@etnetera.cz>
2014-03-09 10:52:59 +0100

    implementace Writes[Entry]

--- app/model/EntryWrites.scala
+++ app/model/EntryWrites.scala
@@ -5,16 +5,23 @@ import play.api.libs.json.Json
 import play.api.libs.json.JsValue
 import play.api.libs.json.JsNull
 import play.api.libs.json.JsNumber
+import play.api.libs.functional.syntax._
+import play.api.libs.json.__ //?!?
 
 trait EntryWrites {
 
-  implicit val entryWrites: Writes[Entry] = new Writes[Entry] {
+  implicit val entryWrites: Writes[Entry] = (
+    (__ \ "id").format[Option[Long]] and
+    (__ \ "body").format[String])(Entry.apply, unlift(Entry.unapply))
 
-    override def writes(e: Entry): JsValue = {
-      Json.obj(
-        "id" -> e.id,
-        "body" -> e.body)
-    }
-  }
+  // alternative non-DSL solution  
+  //  implicit val entryWrites: Writes[Entry] = new Writes[Entry] {
+  //
+  //    override def writes(e: Entry): JsValue = {
+  //      Json.obj(
+  //        "id" -> e.id,
+  //        "body" -> e.body)
+  //    }
+  //  }
 
 }
